dda0f59f2375b09ad9d8d37e5d87525f24a294b1
CarpentierChristopheDev <45299251+CarpentierChristopheDev@users.noreply.github.com>
2026-08-03 10:36:52 +0200

    json

---
+++ json/Program.cs
@@ -0,0 +1,35 @@
+﻿using System.Text.Json; // Package natif
+
+namespace JSON;
+
+public class Client
+{
+    public string? nom {get; set;}
+    public string? prenom {get; set;} 
+    
+}
+
+public class Program {
+    public static void Main()
+    {
+        
+        Console.WriteLine("Test sérialisation/désérialisation JSON");
+        Client? c1 = new Client();
+
+        string jsonStringIn = "{\"nom\":\"Carpentier\",\"prenom\":\"Christophe\"}"; 
+        Console.WriteLine(jsonStringIn); 
+
+        Console.WriteLine("- Désérialisation de la chaine JSON vers l'objet Client"); 
+        c1 = JsonSerializer.Deserialize<Client>(jsonStringIn);
+
+        Console.WriteLine("- Lecture des données de l'instance de Client");
+        Console.WriteLine($"{c1?.nom} {c1?.prenom}"); 
+
+        Console.WriteLine("- Sérialisation de l'instance de Client en chaine JSON"); 
+        string jsonStringOut = JsonSerializer.Serialize(c1);
+        Console.WriteLine(jsonStringOut);
+
+    }
+
+    
+}
