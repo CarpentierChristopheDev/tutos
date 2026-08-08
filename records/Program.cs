715695bb4294f75f9f6bbe3578b6c55d05207d06
CarpentierChristopheDev <45299251+CarpentierChristopheDev@users.noreply.github.com>
2026-08-08 17:13:15 +0200

    Add files via upload
    Add Identities test

--- records/Program.cs
+++ records/Program.cs
@@ -12,7 +12,22 @@ class Program
         Client c1 = new Client();
         c1.Nom = "Test";
         c1.Prenom = "Test2";
-        Console.WriteLine(c1.ToString());
+        Console.WriteLine(c1.ToString()); // ToString gratuit pour débug
+
+        Client c2 = new Client();
+        c2.Nom = "Test";
+        c2.Prenom = "Test2";
+        Console.WriteLine(c2.ToString());
+
+        // Test l"égalité des données
+        if(c1 == c2)
+        {
+            Console.WriteLine("Données OK");
+        }
+        else
+        {
+            Console.WriteLine("Données KO");
+        }
     }
 }
 
