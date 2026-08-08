5454441fb059f4ffc9032420882193a9a5c6d390
CarpentierChristopheDev <45299251+CarpentierChristopheDev@users.noreply.github.com>
2026-08-08 16:35:20 +0200

    Add files via upload

---
+++ records/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace TestRecord;
+
+record Client
+{
+    public string Nom {get; set; } = "";
+    public string Prenom {get; set;} = "";
+}
+class Program
+{
+    public static void Main()
+    {
+        Client c1 = new Client();
+        c1.Nom = "Test";
+        c1.Prenom = "Test2";
+        Console.WriteLine(c1.ToString());
+    }
+}
+
